fb1b47ab26393782a5909b0615d9fa2a576bc48d
ManjunathHebballi <48060862+ManjunathHebballi@users.noreply.github.com>
2020-10-21 16:00:47 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const reasonInput = document.querySelector('#input-reason');
 const amountInput = document.querySelector('#input-amount');
 const clearBtn = document.querySelector('#btn-clear');
 const addBtn = document.querySelector('#btn-add');
+const expenseList = document.querySelector('#expense-list');
+
 
 addBtn.addEventListener('click',() =>{
-console.log("its work!");
+const enterdReason = reasonInput.value;
+const enterdAmount = amountInput.value;
+if(enterdReason.trim().length <=0 ||
+enterdAmount <=0 ||
+enterdAmount.trim().length <=0){
+    return;
+}
+const newItem = document.createElement('ion-item');
+newItem.textContent = enterdReason + ' : ₹ ' + enterdAmount;
+expenseList.appendChild(newItem);
+
 });
