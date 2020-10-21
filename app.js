b66af14def964f5fef1c0a10cd5f795347218f6f
ManjunathHebballi <48060862+ManjunathHebballi@users.noreply.github.com>
2020-10-21 15:39:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const reasonInput = document.querySelector('#input-reason');
+const amountInput = document.querySelector('#input-amount');
+const clearBtn = document.querySelector('#btn-clear');
+const addBtn = document.querySelector('#btn-add');
+
+addBtn.addEventListener('click',() =>{
+console.log("its work!");
+});
